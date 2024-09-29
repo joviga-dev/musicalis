fa8c672db6205aa04a579e7cc7bd560a005fd4c0
joaogarcianlhpd <joaogarciasilva9@gmail.com>
2024-09-28 22:45:03 -0300

    commit inicial

---
+++ src/main/java/br/com/musicalis/musicalis/MusicalisApplication.java
@@ -0,0 +1,13 @@
+package br.com.musicalis.musicalis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicalisApplication.class, args);
+	}
+
+}
