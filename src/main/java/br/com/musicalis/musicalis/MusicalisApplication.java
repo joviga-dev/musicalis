ce6f2771ccfba2192c677f67b7db3be22237e559
joaogarcianlhpd <joaogarciasilva9@gmail.com>
2024-10-26 10:30:36 -0300

    Security comentado

--- src/main/java/br/com/musicalis/musicalis/MusicalisApplication.java
+++ src/main/java/br/com/musicalis/musicalis/MusicalisApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.lang.NonNull;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,10 +22,15 @@ public class MusicalisApplication {
 			@Override
 			public void addCorsMappings(@NonNull CorsRegistry registry) {
 				registry
-				.addMapping("/**")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-				.allowedOrigins("http://localhost:4200");
+						.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedOrigins("http://localhost:4200");
 			}
 		};
 	}
+//@TODO Security
+//	@Bean
+//	public PasswordEncoder encoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 }
---
+++ src/main/java/br/com/musicalis/musicalis/service/SecurityService.java
@@ -0,0 +1,22 @@
+package br.com.musicalis.musicalis.service;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.Data;
+//@TODO Security
+@Data
+@Service
+public class SecurityService {
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    public String criptografar(String password) {
+//        return passwordEncoder.encode(password);
+//    }
+//
+//    public boolean autenticar(String password, String encodedPassword) {
+//        return passwordEncoder.matches(password, encodedPassword);
+//    }
+}
